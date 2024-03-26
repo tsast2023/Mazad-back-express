dd18014afdbc402f3ffec07a5dd62b6fe800d70e
unknown <tsast@gmail.com>
2024-03-26 12:35:46 +0100

    add redis

--- index.js
+++ index.js
@@ -2,56 +2,48 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const bidRoute = require('./routes/BidRoute');
-const { createAdapter } = require('@socket.io/redis-adapter');
-const { createClient } = require('redis');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const { initializeSocket } = require('./socket');
+const session = require('express-session')
+const Redis = require('ioredis')
+const RedisStore = require('connect-redis').default;
+
+
+
+
+
 
 const app = express();
 const server = http.createServer(app);
+const redisClient = new Redis();
+require('dotenv').config();
+
+
 
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://tsast2023:ydNrpqZADUIYJP3y@cluster0.b7tqviv.mongodb.net/MAZAD?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database connected'))
     .catch(err => console.error('Database connection error:', err));
 
-// Create a Redis client
-const redisClient = createClient({
-    host: 'localhost',
-    port: 6379,
-});
 
-// Handle Redis client errors
-redisClient.on('error', function (err) {
-    console.error('Redis client error:', err);
-});
 
-// Create a Redis adapter using the Redis client
-const redisAdapter = createAdapter(redisClient);
-
-// Use the Redis adapter with Socket.IO
-const io = new Server(server , {adapter:redisAdapter});
-
-// Socket.IO event handling
-io.on('connection', function(socket) {
-    console.log('Connected');
-    console.log(`User connected: ${socket.id}`);
-
-    socket.on('msg_from_client', function(from, msg) {
-        console.log(`Message from user ${socket.id} ` + from, msg);
-        io.to(socket.id).emit('message', 'Hello, specific user!');
-    });
-
-    socket.on('establish_connection', function(token) {
-        const userToken = token.accesstoken;
-        console.log("userToken");
-    });
-
-    socket.on('disconnect', function(msg) {
-        console.log("deleting user");
-    });
-});
-
+initializeSocket(server)
+app.use(
+    session({
+      secret: "Session",
+      credentials: true,
+      name: "sid",
+      store: new RedisStore({client:redisClient}),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
+        httpOnly: true,
+        expires: 1000 * 60 * 60 * 24 * 7,
+        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+      },
+    })
+  );
 // Routes
 app.use('/bid', bidRoute);
 
--- socket.js
+++ socket.js
@@ -1,31 +1,31 @@
 const jwt = require('jsonwebtoken');
-
+const {Server} = require('socket.io')
 
 let io; // Define io outside of the function to make it accessible in the exports
 
-function initializeSocket(server, redisAdapter) {
-  const io = require("socket.io")(server, { adapter: redisAdapter });
+function initializeSocket(server) {
+// Use the Redis adapter with Socket.IO
+const io = new Server(server);
 
-  io.on('connection', function(socket) {
-      console.log('Connected');
-      console.log(`User connected: ${socket.id}`);
+// Socket.IO event handling
+io.on('connection', function(socket) {
+    console.log('Connected');
+    console.log(`User connected: ${socket.id}`);
 
-      socket.on('msg_from_client', function(from, msg) {
-          console.log(`Message from user ${socket.id} ` + from, msg);
-          io.to(socket.id).emit('message', 'Hello, specific user!');
-      });
+    socket.on('msg_from_client', function(from, msg) {
+        console.log(`Message from user ${socket.id} ` + from, msg);
+        io.to(socket.id).emit('message', 'Hello, specific user!');
+    });
 
-      socket.on('establish_connection', function(token) {
-          const userToken = token.accesstoken;
-          console.log(userToken);
-        
-      });
+    socket.on('establish_connection', function(token) {
+        const userToken = token.accesstoken;
+        console.log("userToken");
+    });
 
-      socket.on('disconnect', function(msg) {
-          console.log("deleting user");
-          
-      });
-  });
+    socket.on('disconnect', function(msg) {
+        console.log("deleting user");
+    });
+});
 }
 
 module.exports = {
